260e1233a158a6fdc4cf62a1538cbd4a8757fdeb
Ian <jameslin22jameslin@outlook.com>
2019-09-05 10:54:06 +0800

    initial

--- pysot/models/fcos/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-from pysot.models.fcos.fcos import build_fcos
-
-def get_fcos(cfg, in_channels):
-    return build_fcos(cfg, in_channels)
